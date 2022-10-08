258831c700ce6c2527f6de88315f7f1f2af118cb
emanuelefavero <emafaux89@gmail.com>
2022-10-08 17:29:13 +0200

    add comments to show how to add type to useState

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,10 +1,27 @@
+import { FC } from 'react'
 export interface Props {
   name: string
   // optional prop 'color'
   color?: string
 }
 
-function Hello(props: Props) {
+// FC is a type that represents a function component
+// Inside which we are passing in the Props interface
+const Hello: FC<Props> = (props) => {
+  // const [number, setNumber] = useState(0)
+  // const [string, setString] = useState('')
+  // const [arrayOfNumbers, setArrayOfNumbers] = useState<number[]>([])
+  // const [arrayOfStrings, setArrayOfStrings] = useState<string[]>([])
+  // const [arrayOfStringsAndNumbers, setArrayOfStringsAndNumbers] = useState<
+  //   (string | number)[]
+  // >([])
+  // interface IObject {
+  //   name: string
+  //   age?: number
+  // }
+  // const [object, setObject] = useState<IObject>({ name: '' })
+  // ALSO: <IObject | null>(null)
+
   return (
     <>
       <h1 style={props.color ? { color: props.color } : { color: 'inherit' }}>
